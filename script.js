5592c6ddab87993a4ac320694641edf5b678e993
venkrt-designs <venkat.graphicdesign@gmail.com>
2025-03-08 18:49:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,8 +7,3 @@ function closeWindow(windowName) {
     document.getElementById(windowName + '-window').style.display = 'none';
     document.getElementById('overlay').style.display = 'none';
 }
-
-<script>
-document.body.style.background = "url('https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExODh2aWhsOTJtcmI5bjBoeXk0YjdodTh5bzJiZDMxanN5ZWd2YzhmeCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/a692I53FP9XtvM4YCc/giphy.gif') no-repeat center center fixed";
-document.body.style.backgroundSize = "cover";
-</script>
